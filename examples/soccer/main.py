9fad8d5a493687f9fddf9f6227e785e1445d3998
SkalskiP <piotr.skalski92@gmail.com>
2024-07-24 17:17:31 +0200

    goalkeepers team assignment based on team centroid proximity

--- examples/soccer/main.py
+++ examples/soccer/main.py
@@ -111,6 +111,39 @@ def get_crops(frame: np.ndarray, detections: sv.Detections) -> List[np.ndarray]:
     return [sv.crop_image(frame, xyxy) for xyxy in detections.xyxy]
 
 
+def resolve_goalkeepers_team_id(
+    players: sv.Detections,
+    players_team_id: np.array,
+    goalkeepers: sv.Detections
+) -> np.ndarray:
+    """
+    Resolve the team IDs for detected goalkeepers based on the proximity to team
+    centroids.
+
+    Args:
+        players (sv.Detections): Detections of all players.
+        players_team_id (np.array): Array containing team IDs of detected players.
+        goalkeepers (sv.Detections): Detections of goalkeepers.
+
+    Returns:
+        np.ndarray: Array containing team IDs for the detected goalkeepers.
+
+    This function calculates the centroids of the two teams based on the positions of
+    the players. Then, it assigns each goalkeeper to the nearest team's centroid by
+    calculating the distance between each goalkeeper and the centroids of the two teams.
+    """
+    goalkeepers_xy = goalkeepers.get_anchors_coordinates(sv.Position.BOTTOM_CENTER)
+    players_xy = players.get_anchors_coordinates(sv.Position.BOTTOM_CENTER)
+    team_0_centroid = players_xy[players_team_id == 0].mean(axis=0)
+    team_1_centroid = players_xy[players_team_id == 1].mean(axis=0)
+    goalkeepers_team_id = []
+    for goalkeeper_xy in goalkeepers_xy:
+        dist_0 = np.linalg.norm(goalkeeper_xy - team_0_centroid)
+        dist_1 = np.linalg.norm(goalkeeper_xy - team_1_centroid)
+        goalkeepers_team_id.append(0 if dist_0 < dist_1 else 1)
+    return np.array(goalkeepers_team_id)
+
+
 def run_pitch_detection(source_video_path: str, device: str) -> Iterator[np.ndarray]:
     """
     Run pitch detection on a video and yield annotated frames.
@@ -235,15 +268,21 @@ def run_team_classification(source_video_path: str, device: str) -> Iterator[np.
         detections = tracker.update_with_detections(detections)
 
         players = detections[detections.class_id == PLAYER_CLASS_ID]
+        crops = get_crops(frame, players)
+        players_team_id = team_classifier.predict(crops)
+
         goalkeepers = detections[detections.class_id == GOALKEEPER_CLASS_ID]
+        goalkeepers_team_id = resolve_goalkeepers_team_id(
+            players, players_team_id, goalkeepers)
+
         referees = detections[detections.class_id == REFEREE_CLASS_ID]
 
-        crops = get_crops(frame, players)
-        team_id = team_classifier.predict(crops).tolist()
-
         detections = sv.Detections.merge([players, goalkeepers, referees])
-        custom_color_lookup = team_id + [2] * len(goalkeepers) + [3] * len(referees)
-        custom_color_lookup = np.array(custom_color_lookup)
+        custom_color_lookup = np.array(
+                players_team_id.tolist() +
+                goalkeepers_team_id.tolist() +
+                [REFEREE_CLASS_ID] * len(referees)
+        )
         labels = [str(tracker_id) for tracker_id in detections.tracker_id]
 
         annotated_frame = frame.copy()
