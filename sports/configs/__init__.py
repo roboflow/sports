25e7afe263ebf1f64ab587cd8e7a8bfa9204253d
sousousore1 <souichi.saitou@gmail.com>
2026-04-30 23:25:44 +0900

    Add handball court configuration

---
+++ sports/configs/__init__.py
@@ -0,0 +1,7 @@
+from sports.configs.handball import HandballCourtConfiguration
+from sports.configs.soccer import SoccerPitchConfiguration
+
+__all__ = [
+    "HandballCourtConfiguration",
+    "SoccerPitchConfiguration",
+]
